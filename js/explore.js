2882046778910900f053e5cafd10afa0a43254f5
iand11 <ian.driscoll.o@gmail.com>
2017-02-18 15:43:01 -0800

    added psuedocode

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+//take the string and convert it into an array
+//create a empty new array
+//fill the empty array with the items from the string array
+//insert the items at 0 index of the new array 
+//print the new array as a string 
