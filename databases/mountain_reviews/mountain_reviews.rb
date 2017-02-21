8df0818f4b30d7db985c5fa9d28c7919965b7835
iand11 <ian.driscoll.o@gmail.com>
2017-02-21 14:53:32 -0800

    added users table

---
+++ databases/mountain_reviews/mountain_reviews.rb
@@ -0,0 +1,46 @@
+require 'sqlite3'
+require 'faker'
+
+db = SQLite3::Database.new("reviews.rb")
+
+# create_table_cmd = <<-SQL
+# 	CREATE TABLE IF NOT EXISTS users (
+# 		id INTEGER PRIMARY KEY,
+# 		name VARCHAR(255),
+# 		email VARCHAR(255),
+# 		phone INT
+# 	)
+# SQL 
+
+create_table_users = <<-SQL
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		email VARCHAR(255),
+		phone INT
+	)
+SQL
+
+create_table_reviews = <<-SQL 
+	CREATE TABLE IF NOT EXISTS reviews (
+		id INTEGER PRIMARY KEY,
+		user_id INT,
+		mountain_id INT,
+		date DATETIME,
+		snowing BOOLEAN,
+		powder BOOLEAN,
+		ice BOOLEAN,
+		long lines BOOLEAN,
+		sunshine BOOLEAN,
+		comments VARCHAR(255)
+	)
+SQL
+
+create_table_mountains = <<-SQL
+	CREATE TABLE IF NTO EXISTS mountains (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(255)
+	)
+SQL
+
+db.execute(create_table_users)
