958f505a1431bcfe4067cb04c1dea114f2789da3
iand11 <ian.driscoll.o@gmail.com>
2017-02-21 15:18:10 -0800

    added northern california mountains

--- databases/mountain_reviews/mountain_reviews.rb
+++ databases/mountain_reviews/mountain_reviews.rb
@@ -3,15 +3,6 @@ require 'faker'
 
 db = SQLite3::Database.new("reviews.rb")
 
-# create_table_cmd = <<-SQL
-# 	CREATE TABLE IF NOT EXISTS users (
-# 		id INTEGER PRIMARY KEY,
-# 		name VARCHAR(255),
-# 		email VARCHAR(255),
-# 		phone INT
-# 	)
-# SQL 
-
 create_table_users = <<-SQL
 	CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY,
@@ -43,6 +34,26 @@ create_table_mountains = <<-SQL
 	)
 SQL
 
-db.execute(create_table_users)
-db.execute(create_table_reviews)
-db.execute(create_table_mountains)
+# db.execute(create_table_users)
+# db.execute(create_table_reviews)
+# db.execute(create_table_mountains)
+
+def add_user(db, name, email, phone)
+	db.execute("INSERT INTO users (name, email, phone) VALUES (?,?,?)", [name, email, phone])
+end 
+
+# add_user(db, "ian", "ian.driscoll.o@gmail.com", 5166410404)
+
+def add_mountains(db, mountain_name)
+	db.execute("INSERT INTO mountains (name) VALUES (?)", [mountain_name])
+end 
+# add_mountains(db, "Kirkwood")
+# add_mountains(db, "Heavenly")
+# add_mountains(db, "Northstar")
+# add_mountains(db, "Squaw")
+
+norther_california_mountains = ['Homewood', 'Sierra-at-Tahoe', 'Tahoe Donner', 'Soda Springs', 'Bear Valley', 'Sugar Bowl', 'Boreal Mountain', 'Mammoth', 'China Peak']
+norther_california_mountains.each do |item|
+	add_mountains(db, item)
+end 
+
