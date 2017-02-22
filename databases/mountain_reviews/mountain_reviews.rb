94cf37cd873f6e6c75cd82cf944cf5891bb96208
iand11 <ian.driscoll.o@gmail.com>
2017-02-21 16:17:37 -0800

    added driver code to get user info

--- databases/mountain_reviews/mountain_reviews.rb
+++ databases/mountain_reviews/mountain_reviews.rb
@@ -38,7 +38,7 @@ SQL
 
 # db.execute(create_table_users)
 # db.execute(create_table_reviews)
-db.execute(create_table_mountains)
+# db.execute(create_table_mountains)
 
 def add_user(db, name, email, phone)
 	db.execute("INSERT INTO users (name, email, phone) VALUES (?,?,?)", [name, email, phone])
@@ -55,21 +55,46 @@ end
 # add_mountains(db, "Squaw")
 
 norther_california_mountains = ['Kirkwood', 'Heavenly', 'Northstar', 'Squaw', 'Homewood', 'Sierra_at_Tahoe', 'Tahoe_Donner', 'Soda_Springs', 'Bear_Valley', 'Sugar_Bowl', 'Boreal_Mountain', 'Mammoth', 'China_Peak']
-norther_california_mountains.each do |item|
-	add_mountains(db, item)
-end 
+# norther_california_mountains.each do |item|
+# 	add_mountains(db, item)
+# end 
 
 # puts  db.execute("SELECT * FROM mountains")
 # puts db.execute("SELECT * FROM users")
 
+current_mount = ''
 
+puts "Welcome to Norther California Mountain Reviews"
+puts "\n"
+puts "Please enter your name: "
+user_name = gets.chomp
+puts "\n" 
+puts "Please enter your email: "
+user_email = gets.chomp 
+puts "\n"
+puts "Please enter your 10 digit phone number: "
+user_phone_number = gets.chomp
+puts "\n"
+puts "\n"
+
+add_user(db, user_name, user_email, user_phone_number)
+puts db.execute("SELECT * FROM users") 
+
+
+
+loop do
 puts "Please select a mountain from the list:"
 puts  norther_california_mountains
 current_mount = gets.chomp.capitalize 
+break if norther_california_mountains.include? current_mount
+p '|----------------------------------------------------------------------|'
+end 
 
 
 x = db.execute("SELECT * FROM mountains WHERE name = '#{current_mount}'")
 mountain_number =  x[0][0]
+
+
 
 
 
